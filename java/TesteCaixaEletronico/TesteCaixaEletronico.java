128e36cf0eb477b6c693a3eae3aabd11b20c63f6
Spyware <87285320+Spyware0@users.noreply.github.com>
2023-04-22 15:21:59 -0300

    Update TesteCaixaEletronico.java

--- java/TesteCaixaEletronico/TesteCaixaEletronico.java
+++ java/TesteCaixaEletronico/TesteCaixaEletronico.java
@@ -3,8 +3,8 @@ import com.mycompany.conta.*;
 
 public class TesteCaixaEletronico {
     public static void main(String[] args){
-        Cliente gabriel = new Cliente("623.381.673-41", "Gabriel Luis Dantas Matos");
-        Cliente willian = new Cliente("710.131.264-08", "Willian Walace Silva Cutrim");
+        Cliente gabriel = new Cliente("02468", "Gabriel Luis");
+        Cliente willian = new Cliente("13579", "Willian Walace");
         
         Conta contag = new Conta(1, gabriel, 12345, 2000);
         Conta contaw = new Conta(2, willian, 54321, 1500);
