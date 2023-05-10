d2d61c980e3375ceb5ca23c1ca25edb431cab95b
Spyware <gabrielluisdm13@gmail.com>
2023-05-10 20:47:47 -0300

    add leilao virtual

---
+++ estudoDeCaso2/LeilaoVirtual/Pessoa.java
@@ -0,0 +1,13 @@
+package estudoDeCaso2.LeilaoVirtual;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome; 
+    }
+}
