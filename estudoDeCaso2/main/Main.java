781a27511a76e520eff0c3d0212046db9e73c1f6
Spyware <87285320+Spyware0@users.noreply.github.com>
2023-05-11 10:28:03 -0300

    Update Main.java

--- estudoDeCaso2/main/Main.java
+++ estudoDeCaso2/main/Main.java
@@ -1,7 +1,28 @@
-package estudoDeCaso2.main;
+package com.mycompany.estudodecaso2;
+public class Estudodecaso2 {
 
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa("Gabriel");
+        Pessoa p2 = new Pessoa ("kerlyane");
+        Pessoa p3 = new Pessoa ("andria");
+        
+        /*
+        Lance lance1 = new Lance(p1, 300);
+        Lance lance2 = new Lance (p2, 600 );
+        Lance lance3 = new Lance (p3, 900);
+        */
+        
+        Leilao leilao = new Leilao();
+        leilao.adicionaLote("lote de carros");
+        leilao.adicionaLote("lote de barcos");
+        
+        leilao.lanceLote(0, p3, 600);
+        leilao.lanceLote(0, p1, 1000);
+        leilao.lanceLote(0, p2, 300);
+        
+        leilao.close();
+        
     }
 }
+    
